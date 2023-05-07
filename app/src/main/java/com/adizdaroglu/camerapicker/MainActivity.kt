aab64143c4b80756e9ba5fd9b70495449abaa960
Arda Dizdaroğlu <49514372+ardadizdaroglu@users.noreply.github.com>
2023-05-07 12:34:50 +0300

    first commit
    first commit

---
+++ app/src/main/java/com/adizdaroglu/camerapicker/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.adizdaroglu.camerapicker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
